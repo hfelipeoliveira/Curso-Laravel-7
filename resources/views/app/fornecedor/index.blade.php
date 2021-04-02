182835f7965d25bb6fa67e2ec2888fe4ce0396fa
Helio Felipe de Oliveira <hfelipeoliveira@gmail.com>
2021-04-02 01:25:37 -0300

    @Isset

--- resources/views/app/fornecedor/index.blade.php
+++ resources/views/app/fornecedor/index.blade.php
@@ -7,11 +7,17 @@
 
 @endphp
 
-
-@if(count($fornecedores) > 0 && count($fornecedores) < 10)
-    <h3>Existem alguns fornecedores cadastrados</h3>
-@elseif(count($fornecedores) > 10)
-    <h3>Existem vários fornecedores cadastrados</h3>
-@else
-    <h3>Ainda não existem fornecedores cadastrados</h3>
-@endif
+@isset($fornecedores)
+    Fornecedor: {{ $fornecedores[0]['nome'] }}
+    <br/>
+    Status: {{ $fornecedores[0]['status'] }}
+    <br/>
+    @if(!($fornecedores[0]['status'] == 'S'))
+        Fornecedor Inativo
+    @endif
+    <br/>
+    @unless($fornecedores[0]['status'] == 'S')
+        Fornecedor Inativo
+    @endunless
+@endisset
+<br/>
