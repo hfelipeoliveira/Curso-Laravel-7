fd34674ebc2ce42e98a8d6abbea44367e3bd197d
Helio Felipe de Oliveira <hfelipeoliveira@gmail.com>
2021-04-02 00:26:55 -0300

    Projeto inicial

---
+++ routes/web.php
@@ -0,0 +1,55 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', 'PrincipalController@principal')->name('site.index');
+
+Route::get('/sobre-nos', 'SobreNosController@sobreNos')->name('site.sobrenos');
+
+Route::get('/contato', 'ContatoController@contato')->name('site.contato');
+
+Route::get('/login', function(){ return 'Login'; })->name('site.login');
+
+
+Route::prefix('/app')->group(function() {
+    Route::get('/clientes', function(){ return 'clientes'; })->name('app.clientes');
+    Route::get('/fornecedores', 'FornecedorController@index')->name('app.fornecedores');
+    Route::get('/produtos', function(){ return 'produtos'; })->name('app.produtos');
+});
+
+Route::get('/rota1', function(){
+    echo 'Rota 1';
+})->name('site.rota1');
+
+Route::get('/rota2', function(){
+    return redirect()->route('site.rota1');
+})->name('site.rota2');
+
+//Route::redirect('/rota2', '/rota1');
+
+Route::get(
+    '/contato/{nome}/{categoria_id}', 
+    function(
+        string $nome = 'Desconhecido', 
+        int $categoria_id = 1 // 1 info
+    ){
+        echo "Estamos aqui: $nome - $categoria_id";
+    }
+)->where('categoria_id', '[0-9]+')->where('nome', '[A-Za-z]+');
+
+Route::get('teste/{p1}/{p2}', 'TesteController@teste')->name('teste');
+
+Route::fallback(function(){
+    echo 'A rota acessada não existe. <a href="'.Route('site.index').'">Clique aqui</a> para ser redirecionado';
+});
