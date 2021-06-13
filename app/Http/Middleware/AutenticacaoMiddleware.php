fd06559c1e9773a27dee025ff0e1e6a4505a77b5
Helio Felipe de Oliveira <hfelipeoliveira@gmail.com>
2021-06-13 06:12:46 -0300

    Login

--- app/Http/Middleware/AutenticacaoMiddleware.php
+++ app/Http/Middleware/AutenticacaoMiddleware.php
@@ -15,26 +15,11 @@ class AutenticacaoMiddleware
      */
     public function handle($request, Closure $next, $metedo_autenticacao, $perfil)
     {
-        echo $metedo_autenticacao." - ".$perfil."<br/>";
-
-        if($metedo_autenticacao == 'padrao'){
-            echo "Verificar o usuário e senha no bd ".$perfil."<br/>";
-        }
-        
-        if($metedo_autenticacao == 'ldap'){
-            echo "Verificar o usuário e senha no AD ".$perfil."<br/>";
-        }
-
-        if($perfil == 'visitante'){
-            echo 'Exibir apenas alguns recursos';
-        }else{
-            echo 'Carregar o perfil do banco de dados';
-        }
-
-        if(false){
+        session_start();
+        if (isset($_SESSION['email']) && $_SESSION['email'] != ''){
             return $next($request);
         }else{
-            return Response('Acesso negado! Rota exige autenticação.<br/>');
+            return redirect()->route('site.login', ['erro' => 2]);
         }
         
     }
